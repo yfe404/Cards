9cf51b11a65353915fe5e588cc348be1c1d06dfc
yafeunteun <yannfeunteun@gmail.com>
2018-09-14 15:59:39 +0200

    test: create_deck should create a deck of 20 cards

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,5 +2,9 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+  
 end
