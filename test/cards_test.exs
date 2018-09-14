d14e1c5db69864ff4718a6d09305eb043bbaf1c7
yafeunteun <yannfeunteun@gmail.com>
2018-09-14 16:02:37 +0200

    test: shuffling a deck randomizes it

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -6,5 +6,11 @@ defmodule CardsTest do
     deck_length = length(Cards.create_deck)
     assert deck_length == 20
   end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+  end
+  
   
 end
